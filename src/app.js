154cb2b20e1dcc9e114cab9934faafb723a14a93
soham178 <sohamsarkar552@gmail.com>
2026-03-25 01:41:26 +0530

    add basic files and contents

---
+++ src/app.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+// basic configurations
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
+
+
+// cors configurations
+app.use(cors({
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+    credentials:true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders:["Content-Type", "Authorization"]
+}),
+);
+
+app.get("/", (rep, res) => {
+    res.send("Welcome to basecampy");
+});
+
+export default app;
