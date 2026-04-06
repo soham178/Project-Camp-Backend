ecfd2a443e16013f1a431c453bc15ef09221f45a
soham178 <sohamsarkar552@gmail.com>
2026-04-06 21:57:10 +0530

    all routes and validators of auth

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,16 +1,53 @@
 import {Router} from "express";
-import {registerUser,login, logoutUser} from "../controllers/auth.contollers.js";
+import {
+  registerUser,
+  login, 
+  logoutUser,
+  verifyEmail, 
+  forgotPasswordRequest, 
+  refreshAccessToken,
+  resetForgotPassword, 
+  getCurrentUser, 
+  changeCurrentPassword, 
+  resendEmailVerification} from "../controllers/auth.contollers.js";
 import { validate } from "../middlewares/validator.middleware.js";
-import { userRegisterValidator, userLoginvalidator} from "../validators/index.js";
+import { 
+  userRegisterValidator, 
+  userLoginvalidator, 
+  userForgotPasswordValidator, userResetForgotPasswordValidator, userChangeCurrentPasswordValidator} from "../validators/index.js";
 
 import {verifyJWT} from "../middlewares/auth.middleware.js"
 
 const router = Router();
 
+// unsecured routes
 router.route("/register").post(userRegisterValidator(), validate, registerUser);
 
 router.route("/login").post(userLoginvalidator(), validate, login);
 
+router
+  .route("/verify-email/:verificationToken")
+  .get(verifyEmail);
+
+router
+  .route("/refresh-token")
+  .post(refreshAccessToken);
+
+router
+  .route("/forgot-password")
+  .post(userForgotPasswordValidator(), validate, forgotPasswordRequest);
+
+router
+  .route("/reset-password/:resetToken")
+  .post(userResetForgotPasswordValidator(), validate, resetForgotPassword);
+
+//secure routes
 router.route("/logout").post(verifyJWT, logoutUser);
 
-export default router; 
+router.route("/current-user").post(verifyJWT, getCurrentUser);
+
+router.route("/change-password").post(verifyJWT, userChangeCurrentPasswordValidator(), validate, changeCurrentPassword);
+
+router.route("/resend-email-verification").post(verifyJWT, resendEmailVerification);
+
+export default router;
--- src/validators/index.js
+++ src/validators/index.js
@@ -1,44 +1,62 @@
-import {body} from "express-validator"
-
+import { body } from "express-validator";
 
 const userRegisterValidator = () => {
+  return [
+    body("email")
+      .trim()
+      .notEmpty()
+      .withMessage("Email is required")
+      .isEmail()
+      .withMessage("Email is invalid"),
+    body("username")
+      .trim()
+      .notEmpty()
+      .withMessage("Usrename is required")
+      .isLowercase()
+      .withMessage("Username must be in lower case")
+      .isLength({ min: 3 })
+      .withMessage("Username must be at least 3 characters long"),
+    body("password").trim().notEmpty().withMessage("Password is required"),
+    body("fullName").optional().trim(),
+  ];
+};
+
+const userLoginvalidator = () => {
+  return [
+    body("email").optional().isEmail().withMessage("Email is invalid"),
+    body("password").notEmpty().withMessage("Password is required"),
+  ];
+};
+
+const userChangeCurrentPasswordValidator = () => {
+  return [
+    body("oldPassword").notEmpty().withMessage("Old password is required"),
+    body("newPassword").notEmpty().withMessage("New password is required"),
+  ];
+};
+
+const userForgotPasswordValidator = () => {
     return [
         body("email")
-            .trim()
             .notEmpty()
             .withMessage("Email is required")
             .isEmail()
             .withMessage("Email is invalid"),
-        body("username")
-            .trim()
-            .notEmpty()
-            .withMessage("Usrename is required")
-            .isLowercase()
-            .withMessage("Username must be in lower case")
-            .isLength({min: 3})
-            .withMessage("Username must be at least 3 characters long"),
-        body("password")
-            .trim()
-            .notEmpty()
-            .withMessage("Password is required"),
-        body("fullName")
-            .optional()
-            .trim()
     ]
 }
 
-
-const userLoginvalidator = () => {
+const userResetForgotPasswordValidator = () => {
     return [
-        body("email")
-            .optional()
-            .isEmail()
-            .withMessage("Email is invalid"),
-        body("password")
-           .notEmpty()
-           .withMessage("Password is required") 
+        body("newPassword")
+            .notEmpty()
+            .withMessage("Password is required")
+    ]
+}
 
-    ];
+export {
+  userRegisterValidator,
+  userLoginvalidator,
+  userChangeCurrentPasswordValidator,
+  userForgotPasswordValidator,
+  userResetForgotPasswordValidator,
 };
-
-export { userRegisterValidator, userLoginvalidator }
